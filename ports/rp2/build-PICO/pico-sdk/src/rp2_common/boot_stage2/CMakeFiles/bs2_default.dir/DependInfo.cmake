54615d583923c807fb4c502c76b400fdabef0735
唐睿骞 <ruiqian.tang@chaihuo.org>
2021-07-06 14:44:21 +0800

    add build dir

---
+++ ports/rp2/build-PICO/pico-sdk/src/rp2_common/boot_stage2/CMakeFiles/bs2_default.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/mnt/h/qian/micropython/micropython-spi/micropython/lib/pico-sdk/src/rp2_common/boot_stage2/boot2_w25q080.S" "/mnt/h/qian/micropython/micropython-spi/micropython/ports/rp2/build-PICO/pico-sdk/src/rp2_common/boot_stage2/CMakeFiles/bs2_default.dir/boot2_w25q080.S.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "PICO_BOARD=\"pico\""
+  "PICO_BUILD=1"
+  "PICO_NO_HARDWARE=0"
+  "PICO_ON_DEVICE=1"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/mnt/h/qian/micropython/micropython-spi/micropython/lib/pico-sdk/src/rp2_common/boot_stage2/asminclude"
+  "/mnt/h/qian/micropython/micropython-spi/micropython/lib/pico-sdk/src/rp2040/hardware_regs/include"
+  "/mnt/h/qian/micropython/micropython-spi/micropython/lib/pico-sdk/src/rp2_common/hardware_base/include"
+  "/mnt/h/qian/micropython/micropython-spi/micropython/lib/pico-sdk/src/common/pico_base/include"
+  "generated/pico_base"
+  "/mnt/h/qian/micropython/micropython-spi/micropython/lib/pico-sdk/src/boards/include"
+  "/mnt/h/qian/micropython/micropython-spi/micropython/lib/pico-sdk/src/rp2_common/pico_platform/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
